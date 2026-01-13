b5ade349fcbef2c01a8f6b949da997fa5e899684
Koen Hagen <koen.hagen@anwb.nl>
2026-01-13 09:05:07 +0100

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react(),tailwindcss()],
-    base: '/', // for GitHub Pages
+    base: '/geomatcher/', // for GitHub Pages
 });
