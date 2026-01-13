5df8ba545966a5736f7e823515986827cd444d13
Koen Hagen <koen.hagen@anwb.nl>
2026-01-13 08:58:01 +0100

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#137fec",
+                "background-light": "#f6f7f8",
+                "background-dark": "#101922",
+            },
+            fontFamily: {
+                "sans": ["Plus Jakarta Sans", "sans-serif"]
+            },
+        },
+    },
+    plugins: [],
+};
+
+export default config;
