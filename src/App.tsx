173e005f11d46665e8a31039f1abe14959927bb8
Koen Hagen <koen.hagen@anwb.nl>
2026-01-14 10:15:08 +0100

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
 
     useEffect(() => {
         if (gameComplete) return;
-        fetch('/ranks.csv')
+        fetch(`/geomatcher/ranks.csv`)
             .then(res => res.text())
             .then(text => {
                 const lines = text.trim().split('\n');
